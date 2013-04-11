a1120caa5e364a0a501bc8d974fccf69fe4d9707
Mike Davis <mike@optimizely.com>
2013-04-11 09:23:04 -0700

    Add limit parameter to find.

--- mongomock/__init__.py
+++ mongomock/__init__.py
@@ -196,7 +196,7 @@ class Collection(object):
                 document.update(spec)
             self.insert(document)
 
-    def find(self, spec = None, fields = None, filter = None, sort = None, slow = None, read_preference = None):
+    def find(self, spec = None, fields = None, filter = None, sort = None, slow = None, read_preference = None, limit = None):
         if filter is not None:
             _print_deprecation_warning('filter', 'spec')
             if spec is None:
